ef45389189a99f1fcbb5a4c06c27d68f1b91c0df
Gaurav singh <84321170+gauravsingh0@users.noreply.github.com>
2022-01-31 03:22:59 +0530

    Update users.py

--- SaitamaRobot/modules/users.py
+++ SaitamaRobot/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import SaitamaRobot.modules.sql.users_sql as sql
-from SaitamaRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from SaitamaRobot.modules.sql.users_sql import get_all_users
+import SparrowRobot.modules.sql.users_sql as sql
+from SparrowRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from SparrowRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from SparrowRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
